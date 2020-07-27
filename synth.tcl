cbda3cc2e611e0443a431bf7ca2a01e82a8f520b
Adrian Sampson <adrian@radbox.org>
2020-07-27 13:34:37 -0400

    Try adding implementation

--- synth.tcl
+++ synth.tcl
@@ -1,3 +1,6 @@
+# Run this with `vivado -mode batch -source synth.tcl`.
+# Then see report at `out/FutilBuild.runs/synth_1/main_utilization_synth.rpt`.
+
 # Settings: the output directory and the part number (which is a Zynq
 # XC7Z020, found on our ZedBoard).
 set outdir ./out
@@ -8,6 +11,12 @@ create_project -force -part xc7z020clg484-1 FutilBuild $outdir
 add_files [glob ./*.sv]
 set_property top main [current_fileset]
 
-# Run synthesis.
+# Run synthesis. This is enough to generate the utilization report mentioned
+# above but does not include timing information.
 launch_runs synth_1
 wait_on_run synth_1
+
+# Run implementation to generate bitstream.
+set_property STEPS.PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
+launch_runs impl_1 -to_step write_bitstream
+wait_on_run impl_1
