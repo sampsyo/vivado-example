f0c06b3dc2a95f7d77d6d9388a6107ed1bdced8d
Adrian Sampson <adrian@radbox.org>
2020-07-30 09:06:47 -0400

    Try synthesizing "out-of-context" (OOC)

--- synth.tcl
+++ synth.tcl
@@ -23,14 +23,10 @@ add_files [glob ./*.sv]
 add_files -fileset constrs_1 [glob ./*.xdc]
 set_property top main [current_fileset]
 
+# Create an "out-of-context" fileset that synthesizes a module in isolation.
+create_fileset -blockset -define_from main myooc
+
 # Run synthesis. This is enough to generate the utilization report mentioned
 # above but does not include timing information.
-launch_runs synth_1
-wait_on_run synth_1
-
-# Run implementation to generate a bitstream. This also produces the timing
-# report mentioned above. Removing this step makes things go quite a bit
-# faster if you just need the resource report!
-set_property STEPS.PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
-launch_runs impl_1 -to_step write_bitstream
-wait_on_run impl_1
+launch_runs myooc_synth_1
+wait_on_run myooc_synth_1
